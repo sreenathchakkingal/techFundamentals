808d711bb35bba771787fccaf3ef35c0b83b9c77
sreenathchakkingal <sreenathchakkingal04@gmail.com>
2018-10-11 09:51:12 +0530

    HelloWorld Commit

---
+++ com/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.helloworld;
+
+/**
+ * Created by Kannan on 10/11/2018.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
